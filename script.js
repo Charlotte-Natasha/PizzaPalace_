d3d68e93c65beb9c7300bf4d2cb9a41ba9290392
Charlotte Natasha <natashaonyango97@gmail.com>
2022-03-27 21:26:23 +0200

    added getting data js

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ $(document).ready(function(){
     let orderBtn = $('.orderBtn')
     orderBtn.click (function(){
       let heading =  $(this).closest('.card-footer').find ('p').text();
-      $('.modal-header h5').text(heading)
-        
-        
+      $('.modal-header h5').text(heading) 
     })
+//Getting data 
+let submitBtn= $('#modalSubmit')
+submitBtn.click(function(){
+    let size = $('input[name = size]').val();
+    let crust = $('input[name = crust]').val();
+    let toppings = $('input[name = toppings]').val();
+    let quantity = $('#pizzaQuantity').val();
+    let deliveryFalse = $('#deliveryFalse').val();
+    let deliveryTrue = $('#deliveryTrue').val();
+    console.log(size, crust, toppings, quantity, deliveryFalse, deliveryTrue);
+});
+
+
 })
 
